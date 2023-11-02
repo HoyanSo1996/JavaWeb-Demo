8f741f65b7099c1c3aa75e9170cb1bf3f091437b
KennySo <HoyanSo1996@163.com>
2023-11-03 00:51:08 +0800

    [Chapter03 Servlet] 手动配置Servlet.

---
+++ Chapter03-Servlet/src/main/java/com/omega/demo01/CustomServlet.java
@@ -0,0 +1,36 @@
+package com.omega.demo01;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Class CustomServlet
+ * servlet 的基本操作
+ *
+ * @author KennySo
+ * @date 2023/10/15
+ */
+// @WebServlet("/demo1")
+public class CustomServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {}
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("hello, custom servlet!");
+    }
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+}
