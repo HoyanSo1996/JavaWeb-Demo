96c03d18171fd8dd59a57b4c3289ee3b2f9fb0ba
KennySo <HoyanSo1996@163.com>
2023-11-16 00:55:10 +0800

    [Chapter07 FilterAndListener] filter入门案例.

---
+++ Chapter07-FilterAndListener/src/main/java/com/omega/demo01/FilterDemo.java
@@ -0,0 +1,34 @@
+package com.omega.demo01;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Class FilterDemo
+ *
+ * @author KennySo
+ * @date 2023/11/15
+ */
+@WebFilter("/*")
+public class FilterDemo implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        // 放行前, 对request进行处理
+        System.out.println("filter is invoked when request entered.");
+        // 放行代码
+        filterChain.doFilter(request, response);
+        // 放行后, 对response进行处理
+        System.out.println("filter is invoked when response returned.");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- Chapter07-FilterAndListener/src/main/webapp/index.jsp
+++ Chapter07-FilterAndListener/src/main/webapp/index.jsp
@@ -5,9 +5,9 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+<h1>hello JSP~</h1>
+<%
+    System.out.println("hello jsp.");
+%>
 </body>
 </html>
