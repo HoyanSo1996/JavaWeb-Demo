596349022c073856f7d007e6ff8a3a86ad83a663
KennySo <HoyanSo1996@163.com>
2023-11-19 00:59:17 +0800

    [Chapter08 AJAX] Axios入门案例.

---
+++ Chapter08-AJAX/src/main/java/com/omega/demo03/AxiosServlet.java
@@ -0,0 +1,36 @@
+package com.omega.demo03;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Class AxiosServlet
+ *
+ * @author KennySo
+ * @date 2023/11/19
+ */
+@WebServlet("/axios")
+public class AxiosServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("get...");
+        // 1.接收请求参数
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
+        // 2.响应数据
+        response.getWriter().write("Hello, axios!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("post...");
+        this.doGet(request, response);
+    }
+}
